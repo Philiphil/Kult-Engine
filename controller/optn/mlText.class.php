bc39611653112a7dd301a513f5c1e352bb710539
Philiphil <171012@supinfo.com>
2016-11-25 14:45:39 +0000

    Apply fixes from StyleCI

--- controller/optn/mlText.class.php
+++ controller/optn/mlText.class.php
@@ -1,73 +1,106 @@
 <?php
+
+/*
+ * Kult Engine
+ * PHP framework
+ *
+ * MIT License
+ *
+ * Copyright (c) 2016
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * @package Kult Engine
+ * @author Théo Sorriaux (philiphil)
+ * @copyright Copyright (c) 2016, Théo Sorriaux
+ * @license MIT
+ * @link https://github.com/Philiphil/Kult-Engine
+ */
+
 namespace kult_engine;
 
-class mlText{
-	public $_keys = [];
-	public $_textes = [];
+class mlText
+{
+    public $_keys = [];
+    public $_textes = [];
 
-	public function __construct($fnord=null)
-	{
-		if (!is_null($fnord) )
-		{
-			$this->import($fnord);
-		}
-	}
+    public function __construct($fnord = null)
+    {
+        if (!is_null($fnord)) {
+            $this->import($fnord);
+        }
+    }
 
-	public function add_text($k,$t)
-	{
-		$this->_textes[$k] = $t;
-	}
+    public function add_text($k, $t)
+    {
+        $this->_textes[$k] = $t;
+    }
 
-	public function add_lang($k,$l)
-	{
-		$l = is_array($l) ? $l : [$l];
-		if(isset($this->_keys[$k]))
-		{
-			foreach ($l as $key) {
-				array_push($this->_keys[$k], $key);
-			}
-		}else{
-			$this->_keys[$k] = $l;
-		}
-	}
+    public function add_lang($k, $l)
+    {
+        $l = is_array($l) ? $l : [$l];
+        if (isset($this->_keys[$k])) {
+            foreach ($l as $key) {
+                array_push($this->_keys[$k], $key);
+            }
+        } else {
+            $this->_keys[$k] = $l;
+        }
+    }
 
-	public function export()
-	{
-		$r = [];
-		$r[0] = [];
-		$r[1] = [];
-		foreach ($this->_keys as $key => $value) {
-			$r[0][$key] = $value;
-		}
-		foreach ($this->_textes as $key => $value) {
-			$r[1][$key] = $value;
-		}
-		return json_encode($r);
-	}
+    public function export()
+    {
+        $r = [];
+        $r[0] = [];
+        $r[1] = [];
+        foreach ($this->_keys as $key => $value) {
+            $r[0][$key] = $value;
+        }
+        foreach ($this->_textes as $key => $value) {
+            $r[1][$key] = $value;
+        }
 
-	public function import($fnord)
-	{
-		$fnord = json_decode($fnord);
-		foreach ($fnord[0] as $key => $value) {
-			$this->_keys[$key] = $value;
-		}
-		foreach ($fnord[1] as $key => $value) {
-			$this->_textes[$key] = $value;
-		}
-	}
+        return json_encode($r);
+    }
 
-	public function get($lang=null)
-	{
-		$lang = is_null($lang) ? get_lang() : $lang;
-		$k = null;
-		foreach ($this->_keys as $key => $value) {
-			if(in_array($lang, $value))
-			{
-				$k = $key;
-			}
-		}
-		$k = is_null($k) ? 0 : $k;
-		return $this->_textes[$k];
-	}
+    public function import($fnord)
+    {
+        $fnord = json_decode($fnord);
+        foreach ($fnord[0] as $key => $value) {
+            $this->_keys[$key] = $value;
+        }
+        foreach ($fnord[1] as $key => $value) {
+            $this->_textes[$key] = $value;
+        }
+    }
 
+    public function get($lang = null)
+    {
+        $lang = is_null($lang) ? get_lang() : $lang;
+        $k = null;
+        foreach ($this->_keys as $key => $value) {
+            if (in_array($lang, $value)) {
+                $k = $key;
+            }
+        }
+        $k = is_null($k) ? 0 : $k;
+
+        return $this->_textes[$k];
+    }
 }
--- controller/optn/sqlHelper.class.php
+++ controller/optn/sqlHelper.class.php
@@ -1,64 +1,99 @@
 <?php
+
+/*
+ * Kult Engine
+ * PHP framework
+ *
+ * MIT License
+ *
+ * Copyright (c) 2016
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * @package Kult Engine
+ * @author Théo Sorriaux (philiphil)
+ * @copyright Copyright (c) 2016, Théo Sorriaux
+ * @license MIT
+ * @link https://github.com/Philiphil/Kult-Engine
+ */
+
 namespace kult_engine;
-	class sqlHelper{
 
-		private function table_quoted($table)
-		{
-			return strpos($table, "`") === false ? "`" . $table . "`" : $table;
-		}
+class sqlHelper
+{
+    private function table_quoted($table)
+    {
+        return strpos($table, '`') === false ? '`'.$table.'`' : $table;
+    }
 
-		public function select_int($table,$cat,$id='id')
-		{
-			return "SELECT * FROM " . $this->table_quoted($table) . " WHERE " . $cat . " = :" . $id;
-		}
+    public function select_int($table, $cat, $id = 'id')
+    {
+        return 'SELECT * FROM '.$this->table_quoted($table).' WHERE '.$cat.' = :'.$id;
+    }
 
-		public function select_string($table,$cat,$id='id')
-		{
-			return "SELECT * FROM " . $this->table_quoted($table) . " WHERE " . $cat . " like :" . $id;
-		}
+    public function select_string($table, $cat, $id = 'id')
+    {
+        return 'SELECT * FROM '.$this->table_quoted($table).' WHERE '.$cat.' like :'.$id;
+    }
 
-		public function insert($table,$cat,$id=null)
-		{
-			$cat = is_array($cat) ? $cat : [$cat];
-			$id = is_null($id) ? $cat : $id;
-			$id = is_array($id) ? $id : [$id];
+    public function insert($table, $cat, $id = null)
+    {
+        $cat = is_array($cat) ? $cat : [$cat];
+        $id = is_null($id) ? $cat : $id;
+        $id = is_array($id) ? $id : [$id];
 
-			$r = "INSERT INTO " . $this->table_quoted($table) . " ( `";
+        $r = 'INSERT INTO '.$this->table_quoted($table).' ( `';
 
-			for ($i=0; $i < count($cat)-1 ; $i++) { 
-				$r .= $cat[$i] . "` , `" ;
-			}
-			$r .= $cat[count($cat)-1] . "` ) " ;
+        for ($i = 0; $i < count($cat) - 1; $i++) {
+            $r .= $cat[$i].'` , `';
+        }
+        $r .= $cat[count($cat) - 1].'` ) ';
 
-			$r .= "VALUES ( ";
+        $r .= 'VALUES ( ';
 
-			for ($i=0; $i < count($id)-1 ; $i++) { 
-				$r .= ':'. $id[$i] . " , " ;
-			}
-			$r .= ':'. $id[count($id)-1] . " ) " ;
-			return $r;
-		}
+        for ($i = 0; $i < count($id) - 1; $i++) {
+            $r .= ':'.$id[$i].' , ';
+        }
+        $r .= ':'.$id[count($id) - 1].' ) ';
 
-		public function update_int($table,$haystack,$needle='id',$cat,$id=null)
-		{
+        return $r;
+    }
 
-			$cat = is_array($cat) ? $cat : [$cat];
-			$id = is_null($id) ? $cat : $id;
-			$id = is_array($id) ? $id : [$id];
+    public function update_int($table, $haystack, $needle, $cat, $id = null)
+    {
+        $cat = is_array($cat) ? $cat : [$cat];
+        $id = is_null($id) ? $cat : $id;
+        $id = is_array($id) ? $id : [$id];
 
-			$r = "UPDATE " . $this->table_quoted($table);
-			$r .= " SET `";
-			for( $i= 0 ; $i < count($cat)-1 ; $i++)
-			{
-				$r .= $cat[$i] . "` = :" . $id[$i] . " , `";
-			}
-			$r .= $cat[count($cat)-1] . "` = :" . $id[count($cat)-1];
-			$r .= " WHERE " . $this->table_quoted($haystack) . " = :" .$needle;
-			return $r;
-		}
+        $r = 'UPDATE '.$this->table_quoted($table);
+        $r .= ' SET `';
+        for ($i = 0; $i < count($cat) - 1; $i++) {
+            $r .= $cat[$i].'` = :'.$id[$i].' , `';
+        }
+        $r .= $cat[count($cat) - 1].'` = :'.$id[count($cat) - 1];
+        $r .= ' WHERE '.$this->table_quoted($haystack).' = :'.$needle;
 
-		public function delete($table,$cat,$id='id')
-		{
-			return "DELETE FROM " . $this->table_quoted($table) . " WHERE " . $this->table_quoted($cat) . " = :".$id;
-		}
-	}
+        return $r;
+    }
+
+    public function delete($table, $cat, $id = 'id')
+    {
+        return 'DELETE FROM '.$this->table_quoted($table).' WHERE '.$this->table_quoted($cat).' = :'.$id;
+    }
+}
