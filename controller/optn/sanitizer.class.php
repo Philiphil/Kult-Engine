e65cf6e579307c8ddb2a139ad0185a43ac932680
Philiphil <171012@supinfo.com>
2016-12-15 16:11:14 +0000

    Apply fixes from StyleCI

--- controller/optn/sanitizer.class.php
+++ controller/optn/sanitizer.class.php
@@ -1,38 +1,75 @@
 <?php
 
+/*
+ * Kult Engine
+ * PHP framework
+ *
+ * MIT License
+ *
+ * Copyright (c) 2016
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * @package Kult Engine
+ * @author Théo Sorriaux (philiphil)
+ * @copyright Copyright (c) 2016, Théo Sorriaux
+ * @license MIT
+ * @link https://github.com/Philiphil/Kult-Engine
+ */
+
 namespace kult_engine;
 
 class sanitizer
 {
-	public $_var = false;
+    public $_var = false;
 
-	public function __invoke($fnord)
-	{
-		$this->_var = $fnord;
-		return $this;
-	}
+    public function __invoke($fnord)
+    {
+        $this->_var = $fnord;
 
-	public function out()
-	{
-		return $this->_var;
-	}
+        return $this;
+    }
 
-	public function int()
-	{
-		$this->_var = intval($this->_var);
-		return $this;
-	}
+    public function out()
+    {
+        return $this->_var;
+    }
 
-	public function id()
-	{
-		$this->int();
-		$this->_var = $this->_var > 0 ? $this->_var : false;
-		return $this;
-	}
+    public function int()
+    {
+        $this->_var = intval($this->_var);
 
-	public function text()
-	{
-		$this->_var = htmlspecialchars($this->_var);
-		return $this;
-	}
+        return $this;
+    }
+
+    public function id()
+    {
+        $this->int();
+        $this->_var = $this->_var > 0 ? $this->_var : false;
+
+        return $this;
+    }
+
+    public function text()
+    {
+        $this->_var = htmlspecialchars($this->_var);
+
+        return $this;
+    }
 }
