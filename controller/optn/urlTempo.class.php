716b9373e9b0222a6c6eda746dc6f86972e905fd
Philiphil <171012@supinfo.com>
2017-10-25 14:10:12 +0000

    Apply fixes from StyleCI

--- controller/optn/urlTempo.class.php
+++ controller/optn/urlTempo.class.php
@@ -1,20 +1,56 @@
 <?php
 
+/*
+ * Kult Engine
+ * PHP framework
+ *
+ * MIT License
+ *
+ * Copyright (c) 2016-2017
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * @package Kult Engine
+ * @author Théo Sorriaux (philiphil)
+ * @copyright Copyright (c) 2016-2017, Théo Sorriaux
+ * @license MIT
+ * @link https://github.com/Philiphil/Kult-Engine
+ */
+
 namespace kult_engine;
 
+class urlTempo
+{
+    public function tempo($url, $time, $key = '')
+    {
+        $url = strpos($url, '.php?') === -1 ? $url.'?' : $url.'&';
+        $url .= 'time='.$time;
+        $url .= '&token='.hash('crc32b', $time.$key);
 
-class urlTempo{
-	public function tempo($url, $time,$key=""){
-	    $url = strpos($url,".php?") === -1 ? $url."?" : $url."&";
-	    $url .= "time=".$time;
-	    $url .= "&token=".hash("crc32b",$time.$key);
-	    return $url;
-	}
+        return $url;
+    }
 
-	public function untempo($url,$key)
-	{
-	    preg_match('/time=(.*)&/', $url, $e);
-	    preg_match('/&token=(.*)/', $url, $o);
-	    return hash("crc32b",$e[1].$key) === $o[1];
-	}
+    public function untempo($url, $key)
+    {
+        preg_match('/time=(.*)&/', $url, $e);
+        preg_match('/&token=(.*)/', $url, $o);
+
+        return hash('crc32b', $e[1].$key) === $o[1];
+    }
 }
