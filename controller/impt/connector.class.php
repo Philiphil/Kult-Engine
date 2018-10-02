cc8fba002205acee108a74a427219f158da78d8d
Philiphil <171012@supinfo.com>
2018-10-02 17:49:54 +0200

    almost debug'd
    sessionFactory::coreElement au lieu de session::coreElement
    check pk connector est invoqué deux fois ?

---
+++ controller/impt/connector.class.php
@@ -0,0 +1,10 @@
+<?php
+
+class connector extends kult_engine\connectorFactory
+{
+    public static $_DB_DRIVER = "mysql";
+    public static $_DB_HOST = "localhost";
+    public static $_DB_NAME = "main";
+    public static $_DB_USER = "root";
+    public static $_DB_PASS = "cf051ee4";
+}
