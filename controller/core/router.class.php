227a11f38478fe89fda3776a11a8ff13022f334e
Philiphil <171012@supinfo.com>
2019-07-17 16:15:06 +0200

    Update router.class.php

--- controller/core/router.class.php
+++ controller/core/router.class.php
@@ -46,15 +46,16 @@ abstract class router
     public static $_argex = '$';
     private static $_auto_executor = null;
     public static $_global_routing = 1;
-    public static $_code = 200;
+    public static $_code=200;
+    public static $_routing=0;
 
     public static function setter()
     {
         self::$_method = strtoupper($_SERVER['REQUEST_METHOD']);
-        self::$_asked = strpos($_SERVER['REQUEST_URI'], '?') > -1 ? substr($_SERVER['REQUEST_URI'], 0, strpos($_SERVER['REQUEST_URI'], '?')) : $_SERVER['REQUEST_URI'];
+        self::$_asked = strpos($_SERVER['REQUEST_URI'],"?") > -1 ? substr($_SERVER['REQUEST_URI'],0,strpos($_SERVER['REQUEST_URI'],"?")) : $_SERVER['REQUEST_URI'];
         self::$_a_asked = self::read_asked(self::$_asked);
         self::$_route = [];
-        self::$_code = http_response_code();
+        self::$_code=http_response_code();
     }
 
     public static function destruct()
@@ -65,14 +66,13 @@ abstract class router
     public static function read_asked($brut)
     {
         self::init_required();
-
         return explode('/', substr($brut, 1));
     }
 
-    public static function set_route($route, $func, $method = 'GET', $code = 200)
+    public static function set_route($route, $func, $method = 'GET',$code=200)
     {
         self::init_required();
-        self::$_route[count(self::$_route)] = [$route, $func, strtoupper($method), intval($code)];
+        self::$_route[count(self::$_route)] = [$route, $func, strtoupper($method),intval($code)];
     }
 
     public static function exec()
@@ -84,20 +84,32 @@ abstract class router
                 self::exec_route($route);
             }
         }
-
         foreach (self::$_route as $route) {
-            self::exec_route($route);
+            self::exec_route($route);//200s
         }
+        if(self::$_routing === 0 &&  self::$_code !== 404){
+            self::disable_global_routing(true);
+            self::$_code=404;
+            self::exec();
+        }
+
+
     }
 
     public static function exec_route($route)
     {
         self::init_required();
-        if (($route[2] === self::$_method || $route[2] === '*') &&
-           /* $route[3] === self::$_route*/true) {
+        if (($route[2] === self::$_method || $route[2] === '*' )&&
+            $route[3] === self::$_code ) {
             $tmp = self::is_route_applicable($route[0]);
             if ($tmp !== 0) {
-                call_user_func_array($route[1], $tmp);
+                self::$_routing++;
+                $info = new \ReflectionFunction($route[1]);
+                $t = [];
+                foreach ($info->getParameters() as $key) {
+                    $t[count($t)]=$tmp["$".$key->name];
+                }
+                call_user_func_array($route[1], $t);
             }
         }
     }
@@ -114,28 +126,29 @@ abstract class router
         $translated_route = self::read_asked($route);
         $args = [];
 
+
         if (count($translated_route) > count(self::$_a_asked) ||
-            (count($translated_route) < count(self::$_a_asked)
-            && $translated_route[count($translated_route) - 1] != '*')
+            ( count($translated_route) < count(self::$_a_asked) 
+            && $translated_route[count($translated_route)-1] != "*")
         ) {
             return 0;
         }
 
         for ($i = 0; $i < count($translated_route); $i++) {
-            if ($translated_route[$i] != '*' &&
-                !k\contains(self::$_argex, $translated_route[$i]) &&
-                isset(self::$_a_asked[$i]) &&
+
+            if ($translated_route[$i] != '*' && 
+                !k\contains(self::$_argex, $translated_route[$i]) && 
+                isset(self::$_a_asked[$i]) && 
                 $translated_route[$i] != self::$_a_asked[$i]) {
                 return 0;
             }
             if (k\contains(self::$_argex, $translated_route[$i])) {
-                $args[intval(substr($translated_route[$i], strlen(self::$_argex)))] = isset(self::$_a_asked[$i]) ? self::$_a_asked[$i] : null;
+                $args[$translated_route[$i]] = isset(self::$_a_asked[$i]) ? self::$_a_asked[$i] : null;
             }
         }
-        if ($translated_route[0] === '*') {
+        /*if ($translated_route[0] === '*') {
             return 0;
-        }
-
+        }*/
         return $args;
     }
 }
@@ -144,8 +157,8 @@ abstract class router
     {
         public $_route;
 
-        public function __construct($route, $func, $method = 'GET')
+        public function __construct($route, $func, $method = 'GET', $code=200)
         {
-            router::$_global_route[count(router::$_global_route)] = [$route, $func, $method];
+            router::$_global_route[count(router::$_global_route)] = [$route, $func, $method,intval($code)];
         }
     }
